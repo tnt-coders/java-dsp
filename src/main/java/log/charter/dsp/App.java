94ee8c72cf38e71a5f07dcc2bb30ade31e6ba418
tjwrona <tjwrona1992@gmail.com>
2024-05-03 21:28:29 -0400

    Updated code

--- src/main/java/log/charter/dsp/App.java
+++ src/main/java/log/charter/dsp/App.java
@@ -21,16 +21,14 @@ public class App
 		int bufferSize = 4096;
 		
 		float[] input = new float[bufferSize];
-		float[] hammingWindow = HammingWindow.generate(bufferSize);
 		
 		// Generate the cosine wave
 		for (int i = 0; i < input.length; ++i) {
 		    float t = i / (float)sampleRate;
 		    input[i] = (float)Math.cos(2 * Math.PI * frequency * t);
-		    input[i] *= hammingWindow[i];
 		}
 
-		NoteExtractor noteExtractor = new NoteExtractor(sampleRate);
+		NoteExtractor noteExtractor = new NoteExtractor(bufferSize, sampleRate);
 		
 		float[] frequencies = noteExtractor.frequencies();
 		float[] output = noteExtractor.allocOutput();
@@ -39,7 +37,7 @@ public class App
 		
 		for (int bin = 0; bin < output.length / 2; ++bin) {
 		    float mag = (float)sqrt(output[2 * bin + REAL] * output[2 * bin + REAL] + output[2 * bin + IMAG] * output[2 * bin + IMAG]);
-		    System.out.println("Frequency: " + frequencies[bin] + ": " + mag);
+		    System.out.println("BIN: " + bin + " Frequency: " + frequencies[bin] + ": " + mag);
 		}
 	}
 }
--- src/main/java/log/charter/dsp/NoteExtractor.java
+++ src/main/java/log/charter/dsp/NoteExtractor.java
@@ -6,8 +6,11 @@ public class NoteExtractor {
 	private static final int REAL = 0;
 	private static final int IMAG = 1;
 	
-	// Start at C0
-	private static final float F_MIN = 16.35f;
+	// Declare reference frequency of A4
+	private static final float A4 = 440f;
+	
+	// Go down as low as C0 (57 semitones below A4)
+	private static final float SEMITONES_BELOW_A4 = 57;
 	
 	// Span 8 octaves
 	private static final int OCTAVES = 8;
@@ -19,29 +22,34 @@ public class NoteExtractor {
 	private static final int BIN_COUNT = (int)(NOTE_COUNT * (0.5f / RESOLUTION));
 	private static final int BINS_PER_OCTAVE = (int)(NOTES_PER_OCTAVE * (0.5f / RESOLUTION));
 	
-	private final int sampleRate;
 	private final float[] frequencyBins = new float[BIN_COUNT];
 	
 	private final FourierTransform fft;
+	private final float[] window;
 	private final float[] fftInput;
 	private final float[] fftOutput;
 	
-	NoteExtractor(final int sampleRate) {
-		this(sampleRate, 0);
+	NoteExtractor(final int bufferSize, final int sampleRate) { 
+		this(bufferSize, sampleRate, 0);
 	}
 	
-	NoteExtractor(final int sampleRate, final int centOffset) {
-		this.sampleRate = sampleRate;
+	NoteExtractor(final int bufferSize, final int sampleRate, final int centOffset) {
+		if (bufferSize > sampleRate) {
+			throw new IllegalArgumentException("NoteExtractor input buffer must be less than 1 second in length.");
+		}
 		
 		// Calculate the frequency bins
-		final float ref_frequency = F_MIN * (float)Math.pow(2, centOffset / (NOTES_PER_OCTAVE * 1000f));
+		final float ref_frequency = A4 * (float)Math.pow(2, centOffset / (NOTES_PER_OCTAVE * 1000f));
 		for (int bin = 0; bin < this.frequencyBins.length; ++bin) {
-			frequencyBins[bin] = ref_frequency * (float)Math.pow(2, bin / (float)BINS_PER_OCTAVE);
+			frequencyBins[bin] = ref_frequency * (float)Math.pow(2, (bin - SEMITONES_BELOW_A4 * (0.5f / RESOLUTION)) / (float)BINS_PER_OCTAVE);
 		}
 		
 		// Initialize the FFT with a buffer size equal to the sample rate
 		// This makes the outputs precisely 1Hz apart and simplifies calculations
 		fft = new FourierTransform(sampleRate);
+		
+		// Create a window
+		window = HammingWindow.generate(bufferSize);
 		
 		// Initialize the FFT input (zero pad up to the sample rate)
 		fftInput = new float[fft.inputSize()];
@@ -60,14 +68,14 @@ public class NoteExtractor {
 	}
 	
 	public void execute(float[] input, float[] output) {
-		if (input.length > sampleRate) {
-			throw new IllegalArgumentException("NoteExtractor input signal must be less than 1 second in length");
+		if (input.length != window.length) {
+			throw new IllegalArgumentException("NoteExtractor input size does not match the specified buffer size.");
 		}
 		
 		// Copy the input signal into the FFT input buffer
 		// The buffer is already appropriately zero padded from when it was initialized
 		for (int i = 0; i < input.length; ++i) {
-			fftInput[i] = input[i];
+			fftInput[i] = input[i] * window[i];
 		}
 
 		fft.execute(fftInput, fftOutput);
